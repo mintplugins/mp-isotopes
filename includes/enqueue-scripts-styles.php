92b617a32d90fb139797606f71462b45e06bda94
Phil Johnston <johnstonphilip@gmail.com>
2013-07-26 16:37:29 -0400

    beta1.0.0.8
    Signed-off-by: Phil Johnston <johnstonphilip@gmail.com>

--- includes/enqueue-scripts-styles.php
+++ includes/enqueue-scripts-styles.php
@@ -9,7 +9,7 @@ if ( ! function_exists( 'moveplugins_isotopes_scripts' ) ):
 		}
 		wp_enqueue_style( 'moveplugins_isotopes_animation_css', plugins_url( '/includes/css/animation_style.css', dirname(__FILE__)));
 		wp_enqueue_script( 'moveplugins_isotopes_isotope', plugins_url( '/js/jquery.isotope.min.js', __FILE__ ), array( 'jquery' ) );
-		wp_enqueue_script( 'moveplugins_isotopes_load_isotope', plugins_url( '/js/load_isotope.js', __FILE__ ), array( 'jquery' ) );
+		wp_enqueue_script( 'moveplugins_isotopes_load_isotope', plugins_url( '/js/load_isotope.js', __FILE__ ), array( 'jquery', 'moveplugins_isotopes_isotope' ) );
 	}
 endif; //moveplugins_isotopes_scripts
 add_action( 'wp_enqueue_scripts', 'moveplugins_isotopes_scripts' );
